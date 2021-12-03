9e43fcabaf42098da1169d0f848e93473c8ad388
K Pranit Abhinav <impranitabhinav@gmail.com>
2021-12-03 13:25:15 +0530

    example file updated

--- fullyconnectedNN.py
+++ fullyconnectedNN.py
@@ -3,8 +3,9 @@ from sklearn import datasets
 import numpy as np
 from neural_networks import NeuralNetwork
 from neural_networks.layers import Dense,Activation
-from neural_networks.optimizers import RMS_prop
+from neural_networks.optimizers import Adam
 from neural_networks.utils import SquareLoss
+from sklearn.preprocessing import normalize #machine learning algorithm library
 
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -12,10 +13,11 @@ from sklearn.metrics import accuracy_score
 
 
 data = datasets.load_iris()
-X = data.data[0:90]
-y = data.target[0:90]
-
+X = data.data
+y = data.target
+X=normalize(X,axis=0)
 X, X_test, y , y_test = train_test_split(X, y, test_size=0.33)
+
 
 
 def to_categorical(x, n_col=None):
@@ -30,17 +32,17 @@ n_samples, n_features = X.shape
 n_hidden = 15
 print("no of samples:",n_samples)
 
-optimizer = RMS_prop()
+optimizer = Adam()
 clf = NeuralNetwork(optimizer=optimizer,
                         loss=SquareLoss)
 
 clf.add(Dense(n_hidden, input_shape=(n_features,)))
 clf.add(Activation('leaky_relu'))
-clf.add(Dense(n_hidden))
-clf.add(Dense(2))
+clf.add(Dense(15))
+clf.add(Dense(3))
 clf.add(Activation('softmax'))
 
-train_err = clf.fit(X, y, n_epochs=5, batch_size=16)
+train_err = clf.fit(X, y, n_epochs=50, batch_size=16)
 
 
 # print(train_err)
