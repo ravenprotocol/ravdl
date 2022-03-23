4661a89cf13272794dee96723fefd2747c57f3c2
Unnikrishnan Menon <menon.uk1998@gmail.com>
2022-03-23 13:30:32 +0530

    code revamped and working

--- neural_networks/NeuralNetwork.py
+++ neural_networks/NeuralNetwork.py
@@ -1,77 +1,130 @@
-import ravop.core.c as R
+from __future__ import print_function, division
+from terminaltables import AsciiTable
 import numpy as np
-import time
+# import progressbar
+from ..utils import batch_iterator
+from ..utils.misc import bar_widgets
+import ravop as R
+
 
 class NeuralNetwork():
+    """Neural Network. Deep Learning base model.
 
-    def __init__(self, optimizer, loss):
+    Parameters:
+    -----------
+    optimizer: class
+        The weight optimizer that will be used to tune the weights in order of minimizing
+        the loss.
+    loss: class
+        Loss function used to measure the model's performance. SquareLoss or CrossEntropy.
+    validation: tuple
+        A tuple containing validation data and labels (X, y)
+    """
+    def __init__(self, optimizer, loss, validation_data=None):
         self.optimizer = optimizer
         self.layers = []
         self.errors = {"training": [], "validation": []}
         self.loss_function = loss()
+        # self.progressbar = progressbar.ProgressBar(widgets=bar_widgets)
+
+        self.val_set = None
+        if validation_data:
+            X, y = validation_data
+            self.val_set = {"X": R.t(X), "y": R.t(y)}
 
     def set_trainable(self, trainable):
+        """ Method which enables freezing of the weights of the network's layers. """
         for layer in self.layers:
             layer.trainable = trainable
 
     def add(self, layer):
+        """ Method which adds a layer to the neural network """
+        # If this is not the first layer added then set the input shape
+        # to the output shape of the last added layer
         if self.layers:
             layer.set_input_shape(shape=self.layers[-1].output_shape())
+
+        # If the layer has weights that needs to be initialized 
         if hasattr(layer, 'initialize'):
             layer.initialize(optimizer=self.optimizer)
 
         # Add layer to the network
         self.layers.append(layer)
 
-    def train_on_batch(self, X, y):
-        y_pred = self._forward_pass(X)
-        #print("ypred:",y(),y_pred())
-
-
+    def test_on_batch(self, X, y):
+        """ Evaluates the model over a single batch of samples """
+        y_pred = self._forward_pass(X, training=False)
         loss = R.mean(self.loss_function.loss(y, y_pred))
-        loss.wait_till_computed()
         acc = self.loss_function.acc(y, y_pred)
+
+        return loss, acc
+
+    def train_on_batch(self, X, y):
+        """ Single gradient update over one batch of samples """
+        y_pred = self._forward_pass(X)
+        loss = R.mean(self.loss_function.loss(y, y_pred))
+        # print('   Loss: ', loss())
+        acc = self.loss_function.acc(y, y_pred)
+        # Calculate the gradient of the loss function wrt y_pred
         loss_grad = self.loss_function.gradient(y, y_pred)
-        loss_grad.wait_till_computed()
-        #print(loss_grad())
+        # print('   Loss Gradient: ', loss_grad())
         # Backpropagate. Update weights
         self._backward_pass(loss_grad=loss_grad)
-        loss.wait_till_computed()
-        return loss(), acc
+
+        return loss, acc
 
     def fit(self, X, y, n_epochs, batch_size):
-        X = R.Tensor(X)
-        y = R.Tensor(y)
-        X.wait_till_computed()
-        n_samples = len(X())
-        for _ in range(n_epochs):
-            print("\n\n\nno. of epoch :",_)
+        """ Trains the model for a fixed number of epochs """
+        X = R.t(X)
+        y = R.t(y)
+        # for _ in self.progressbar(range(n_epochs)):
+        for epoch in range(1, n_epochs + 1):
+            print('\nEpoch: ', epoch)
             batch_error = []
-            for batch in range(0, n_samples, batch_size):
-                begin, end = batch, min(batch + batch_size, n_samples)
-                batch_y=y.slice(begin=begin, size=end - begin)
-                batch_x= X.slice(begin=begin, size=end - begin)
-                batch_x.wait_till_computed()
-                batch_y.wait_till_computed()
-                loss, _ = self.train_on_batch(batch_x, batch_y)
-                batch_error.append(float(loss))
-            print(batch_error,_)
-            self.errors["training"].append(R.mean(R.Tensor(batch_error)))
-        return self.errors["training"]
+            for X_batch, y_batch in batch_iterator(X, y, batch_size=batch_size):
+                loss, _ = self.train_on_batch(X_batch, y_batch)
+                batch_error.append(loss())
+            print("   Batch Error: ", batch_error)
+            self.errors["training"].append(np.mean(batch_error))
+
+            if self.val_set is not None:
+                val_loss, _ = self.test_on_batch(self.val_set["X"], self.val_set["y"])
+                self.errors["validation"].append(val_loss())
+
+        return self.errors["training"], self.errors["validation"]
 
     def _forward_pass(self, X, training=True):
+        """ Calculate the output of the NN """
         layer_output = X
         for layer in self.layers:
             layer_output = layer.forward_pass(layer_output, training)
+
         return layer_output
 
     def _backward_pass(self, loss_grad):
+        """ Propagate the gradient 'backwards' and update the weights in each layer """
         for layer in reversed(self.layers):
             loss_grad = layer.backward_pass(loss_grad)
 
+    def summary(self, name="Model Summary"):
+        # Print model name
+        print (AsciiTable([[name]]).table)
+        # Network input shape (first layer's input shape)
+        print ("Input Shape: %s" % str(self.layers[0].input_shape))
+        # Iterate through network and get each layer's configuration
+        table_data = [["Layer Type", "Parameters", "Output Shape"]]
+        tot_params = 0
+        for layer in self.layers:
+            layer_name = layer.layer_name()
+            params = layer.parameters()()
+            out_shape = layer.output_shape()
+            table_data.append([layer_name, str(params), str(out_shape)])
+            tot_params += params
+        # Print network configuration table
+        print (AsciiTable(table_data).table)
+        print ("Total Parameters: %d\n" % tot_params)
+
     def predict(self, X):
-        X=R.Tensor(X)
-        pred= self._forward_pass(X, training=False)
-        pred.wait_till_computed()
-        print(pred)
-        return pred()
+        """ Use the trained model to predict labels of X """
+        X = R.t(X)
+        return self._forward_pass(X, training=False)
