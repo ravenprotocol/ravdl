31ddf3da7b3f8b8bda7c84c183115f8c829f855d
Anirudh Menon <senseianirudh28@gmail.com>
2022-05-10 23:59:00 +0530

    optimized with new ops and global ops

--- neural_networks/activation_functions.py
+++ neural_networks/activation_functions.py
@@ -1,32 +1,33 @@
 import ravop as R
-import numpy as np
+from ..globals import globals as g
 
 class Sigmoid():
     def __call__(self, x):
-        return R.div(R.t(1), R.add(R.t(1), R.exp(R.neg(x))))
+        return R.div(g.one, R.add(g.one, R.exp(R.neg(x))))
 
     def gradient(self, x):
-        return self.__call__(x) * (R.t(1) - self.__call__(x))
+        return self.__call__(x) * (g.one - self.__call__(x))
 
 class Softmax():
     def __call__(self, x):
-        e_x = R.exp(x - R.t(np.max(x(), axis=-1, keepdims=True)))
-        return R.div(e_x, R.t(np.sum(e_x(), axis=-1, keepdims=True)))
+        e_x = R.exp(x - R.max(x, axis=-1, keepdims="True"))
+        return R.div(e_x, R.sum(e_x, axis=-1, keepdims="True"))
 
     def gradient(self, x):
-        return self.__call__(x) * (R.t(1) - self.__call__(x))
+        return self.__call__(x) * (g.one - self.__call__(x))
 
 class TanH():
     def __call__(self, x):
-        return R.div(R.t(2), R.t(1) + R.exp(R.neg(R.t(2)) * x)) - R.t(1)
+        return R.div(g.two, g.one + R.exp(R.neg(g.two) * x)) - g.one
 
     def gradient(self, x):
-        return R.t(1) - R.pow(self.__call__(x), R.t(2))
+        return g.one - R.pow(self.__call__(x), g.two)
 
 class ReLU():
     def __call__(self, x):
-        x_value = x()
-        return R.t(np.where(x_value >= 0, x_value, 0))
+        condition = R.greater_equal(x, g.zero)
+        return R.where(x,g.zero,condition=condition().tolist())
 
     def gradient(self, x):
-        return R.t(np.where(x() >= 0, 1, 0))
+        condition = R.greater_equal(x, g.zero)
+        return R.where(g.one,g.zero,condition=condition().tolist())
