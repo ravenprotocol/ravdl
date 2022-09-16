b9a479cb58b01a6a2d943a2358977c725daf1a77
Kailash <ahikailash1@gmail.com>
2022-09-16 15:43:08 +0530

    Version updated

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="ravdl",
-    version="0.7",
+    version="0.8",
     license='MIT',
     author="Raven Protocol",
     author_email='kailash@ravenprotocol.com',
