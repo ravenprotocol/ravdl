96e537baf44277e662f65aedda22f7fcc5372ab0
Anirudh Menon <senseianirudh28@gmail.com>
2022-11-07 15:14:33 +0530

    Latest Version check
    Co-authored-by: Unnikrishnan <menon.uk1998@gmail.com>

--- ravdl/utils/misc.py
+++ ravdl/utils/misc.py
@@ -1,6 +1,28 @@
 import numpy as np
 import onnx
 
+import json
+import urllib.request
+from pip._internal.operations.freeze import freeze
+
+def isLatestVersion(pkgName):
+    # Get the currently installed version
+    current_version = ''
+    for requirement in freeze(local_only=False):
+        pkg = requirement.split('==')
+        if pkg[0] == pkgName:
+            current_version = pkg[1]
+
+    # Check pypi for the latest version number
+    contents = urllib.request.urlopen('https://pypi.org/pypi/'+pkgName+'/json').read()
+    data = json.loads(contents)
+    latest_version = data['info']['version']
+
+    # print('Current version of '+pkgName+' is '+current_version)
+    # print('Latest version of '+pkgName+' is '+latest_version)
+
+    return latest_version == current_version
+
 def create_initializer_tensor(
         name: str,
         tensor_array: np.ndarray,
